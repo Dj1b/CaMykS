906a88524216f8d360946a20db23e64af39e2e48
JB Lebrun <jb@ideogram-design.fr>
2017-10-02 12:44:27 +0200

    Release 1.0-20171001

--- engine/data/js/tool/string.js
+++ engine/data/js/tool/string.js
@@ -83,7 +83,17 @@ String.prototype.pad = function(len, str, pos) {
   return v;
 }
 
-
+/*
+ * replace all 
+ * @param string pattern
+ * @param string replacement
+ * @return string
+ */
+String.prototype.replaceAll = function(pattern, replacement) {
+    var target = this;
+    return target.split(pattern).join(replacement);
+};
+ 
 /* 
  * trim string 
  * @access public
@@ -132,3 +142,36 @@ function string_stripTags (string, allowed) {
     return allowed.indexOf('<' + $1.toLowerCase() + '>') > -1 ? $0 : ''
   });
 }
+
+/*
+ * format string number 
+ * @param mixed number
+ * @param integer decimals
+ * @param string decimalsSep
+ * @param string thousandsSep
+ * @return string
+ */
+function string_numberFormat (number, decimals, decimalsSep, thousandsSep) {
+  number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+  var n = !isFinite(+number) ? 0 : +number;
+  var prec = !isFinite(+decimals) ? 0 : Math.abs(decimals);
+  var sep = (typeof thousandsSep === 'undefined') ? ',' : thousandsSep;
+  var dec = (typeof decimalsSep === 'undefined') ? '.' : decimalsSep;
+  var s = '';
+
+  var toFixedFix = function (n, prec) {
+    var k = Math.pow(10, prec);
+    return '' + (Math.round(n * k) / k).toFixed(prec);
+  }
+
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+  if (s[0].length > 3) {
+    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+  }
+  if ((s[1] || '').length < prec) {
+    s[1] = s[1] || '';
+    s[1] += new Array(prec - s[1].length + 1).join('0');
+  }
+
+  return s.join(dec)
+} 
