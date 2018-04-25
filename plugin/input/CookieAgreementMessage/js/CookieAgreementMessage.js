bb41c3bb8b59c57b2a24ee80c20af792a80852b4
JB Lebrun <jb@ideogram-design.fr>
2018-04-25 12:48:42 +0200

    Normalises files format, indents more nicely the code and makes the CookieAgreementMessage input, ready to be parsed by Doxygen.

--- plugin/input/CookieAgreementMessage/js/CookieAgreementMessage.js
+++ plugin/input/CookieAgreementMessage/js/CookieAgreementMessage.js
@@ -1,64 +1,62 @@
-/*
- * CaMykS Engine
- * Developed by	    : camyks.net
- * Author	        : CaMykS Team
- * CaMykS Version   : 1.0
- * Object Version   : 1.0
- * Object Type      : Plugin / Input Javascripts
- * Creation Date    : May 2017
- * Last Modif Date  : Feb 2018
- *
- * CookieAgreementMessage JS Engine
- *
+/**
+ * @brief CookieAgreementMessage Input scripts
+ * @details Plugin / Input Javascripts
+ * @file plugin/input/CookieAgreementMessage/js/CookieAgreementMessage.js
+ * @author CaMykS Team
+ * @version 1.0
+ * @date Creation: May 2017
+ * @date Modification: Apr 2018
+ * @copyright 2017 - 2018 CaMykS Team
+ * @note This program is distributed as is - WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
-
 var CookieAgreementMessage = {
-  params: {},
+    params: {},
 
-  /*
-   * add parameter
-   * @param string name
-   * @param mixed value
-   * @return void
-   * @access public
-   */
-  set_param: function(param, value, subvalue) {
-    if ( subvalue != undefined && this.params[param] )
-      this.params[param][value] = subvalue;
-    else
-      this.params[param] = value;
-  },
-   
-  /*
-   * return param value from name
-   * @param mixed param
-   * @return mixed
-   * @access public
-   */
-  get_param: function(param, value) {
-    if (value != undefined && (this.params[param][value] || this.params[param][value] === 0))
-        return this.params[param][value];
-    if (value == undefined && this.params[param] || this.params[param] === 0)
-      return this.params[param]
-    return false;
-  },
-  
-  /* 
-   * initialise object
-   * @return void
-   * @access public
-   */
-  initialise: function() {
-    
-  },
-  
-  /*
-   * go to given anchor
-   * @param string anchor
-   * @access public
-   */
-  accept_cookies: function() {
-    cookie_save('cookieAccepted', 1, this.get_param('cookieTTL'));
-    document.getElementById('cookieAgreementMessage').style.display = 'none';
-  },
+    /*
+     * add parameter
+     * @param string name
+     * @param mixed value
+     * @return void
+     * @access public
+     */
+    set_param: function(param, value, subvalue) {
+        if ( subvalue != undefined && this.params[param] )
+            this.params[param][value] = subvalue;
+        else
+            this.params[param] = value;
+    },
+
+    /*
+     * return param value from name
+     * @param mixed param
+     * @return mixed
+     * @access public
+     */
+    get_param: function(param, value) {
+        if (value != undefined && (this.params[param][value] || this.params[param][value] === 0))
+                return this.params[param][value];
+        if (value == undefined && this.params[param] || this.params[param] === 0)
+            return this.params[param]
+        return false;
+    },
+
+    /*
+     * initialise object
+     * @return void
+     * @access public
+     */
+    initialise: function() {
+
+    },
+
+    /*
+     * go to given anchor
+     * @param string anchor
+     * @access public
+     */
+    accept_cookies: function() {
+        cookie_save('cookieAccepted', 1, this.get_param('cookieTTL'));
+        document.getElementById('cookieAgreementMessage').style.display = 'none';
+    },
 }
