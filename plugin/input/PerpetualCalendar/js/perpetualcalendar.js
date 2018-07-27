2ea316e824abc1798dc92e4ee0019e3102066a83
JB Lebrun <jb@ideogram-design.fr>
2018-07-27 16:55:27 +0200

    Normalises files format, indents more nicely the code and makes the PerpetualCalendar input, ready to be parsed by Doxygen.

--- plugin/input/PerpetualCalendar/js/perpetualcalendar.js
+++ plugin/input/PerpetualCalendar/js/perpetualcalendar.js
@@ -1,16 +1,16 @@
-/*
- * CaMykS Engine
- * Developed by		  : camyks.net
- * Author			      : CaMykS Team <camyks.contact@gmail.com>
- * CaMykS Version   : 1.0b1
- * Object Version	  : 1.0
- * Object Type      : Plugin / Module Javascript
- * Create Date		  : Apr 2010
- * Last Modif Date	: Apr 2010
- * 
- * Perpetual Calendar engine script
+/**
+ * @brief PerpetualCalendar Input scripts
+ * @details Plugin / Input Javascripts
+ * @file plugin/input/PerpetualCalendar/js/PerpetualCalendar.js
+ * @author CaMykS Team
+ * @version 1.0
+ * @date Creation: Apr 2010
+ * @date Modification: Jul 2018
+ * @copyright 2010 - 2018 CaMykS Team
+ * @note This program is distributed as is - WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
- 
+
 /* main object */
 function PerpetualCalendar(name) {
   this.name = name;
@@ -18,7 +18,7 @@ function PerpetualCalendar(name) {
   this.params = {};
   this.locales = {};
   this.items = {};
-  
+
   /*
    * set parameter value
    * @param string name
@@ -33,7 +33,7 @@ function PerpetualCalendar(name) {
     else
       this.params[name] = value;
   };
-  
+
   /*
    * get parameter value
    * @param string name
@@ -48,9 +48,9 @@ function PerpetualCalendar(name) {
       return this.params[name][subname];
     return false;
   };
-  
+
   /*
-   * set locale value 
+   * set locale value
    * @param string name
    * @param string value
    * @return void
@@ -59,9 +59,9 @@ function PerpetualCalendar(name) {
   this.set_locale = function(name, value) {
     this.locales[name] = value;
   };
-  
+
   /*
-   * get locale value 
+   * get locale value
    * @param string name
    * @return void
    * @access public
@@ -71,7 +71,7 @@ function PerpetualCalendar(name) {
       return this.locales[name];
     return name;
   };
-  
+
   /*
    * set item value
    * @param array params
@@ -82,41 +82,41 @@ function PerpetualCalendar(name) {
     if (item['date']) {
       date = new Date(parseInt(item['date']));
       my = date.getFullYear()+'-'+date.getMonth();
-      day = date.getDate();      
+      day = date.getDate();
     } else {/*
       my = params['year']+'-'+params['month'];
       day = parseInt(params['day']);*/
       return;
     }
-    
-    
+
+
     /* add item to item list */
     if (!this.items[my])
       this.items[my] = new Array();
-      
+
     if (!this.items[my][day])
      		this.items[my][day] = new Array();
-    
+
     this.items[my][day][this.items[my][day].length] = item;
 
-    
+
     /* check if master object is loaded */
     if (this.loaded == false)
       return;
-    
+
     /* check if day is in displayed month */
     if (date.getMonth() != this.get_param('date').getMonth()
       || date.getFullYear() != this.get_param('date').getFullYear())
       return;
-      
+
     /* update cell */
     cell = document.getElementById(this.name + 'Cell' + day);
     if (this.items[my][day].length > 1 && this.get_param('multiItem')['enabled'] == 1)
     	this._set_cellProperties(cell, this.get_param('multiItem'), day);
     else
-      this._set_cellProperties(cell, item, day);
+        this._set_cellProperties(cell, item, day);
   };
-  
+
   /*
    * remove items from params
    * @param Date date
@@ -126,13 +126,13 @@ function PerpetualCalendar(name) {
    */
   this.remove_items = function(date, day) {
   	my = date.getFullYear()+'-'+date.getMonth();
-  	
+
   	if (day == undefined)
   		this.items[my] = new Array();
   	else
   		this.items[my][day] = new Array();
   }
-  
+
   /*
    * get item value
    * @param Date date
@@ -144,12 +144,12 @@ function PerpetualCalendar(name) {
     my = date.getFullYear()+'-'+date.getMonth();
     if (day == undefined)
       day = date.getDate();
-        
+
     if (this.items[my] && this.items[my][day])
       return this.items[my][day][0];
     return false;
   };
-  
+
   /*
    * get items value
    * @param Date date
@@ -161,35 +161,35 @@ function PerpetualCalendar(name) {
     my = date.getFullYear()+'-'+date.getMonth();
     if (day == undefined)
       day = date.getDate();
-        
+
     if (this.items[my] && this.items[my][day])
       return this.items[my][day];
     return false;
   };
-    
-  /* 
+
+  /*
    * initialise object
    * @return void
    * @access public
    */
   this.initialise = function() {
-  
+
     /* check calendar DOM element */
     if (document.getElementById(this.name+'Object')) {
       this.set_param('calendarObject', document.getElementById(this.name+'Object'));
       this.set_param('calendarDate', document.getElementById(this.name+'Date'));
     } else
       return;
-  
+
     /* save plateform */
     this.set_param('plateform', navigator.userAgent.toLowerCase().indexOf('msie')==-1 ? 'other':'ie');
-  
+
     /* build month list */
     this.set_param('monthes', new Array('january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'));
-  
+
     /* build day list */
     this.set_param('days', new Array('monday', 'tuesday', 'wednesday', 'thurday', 'friday', 'saturday', 'sunday'));
-  
+
     /* define default date */
     d = new Date();
     d.setDate(1);
@@ -198,11 +198,11 @@ function PerpetualCalendar(name) {
     if (this.get_param('dateYear') !== false)
       d.setYear(this.get_param('dateYear'));
     this.set_param('date', d);
-   
+
     /* set calendar as loaded */
     this.loaded = true;
   };
-  
+
   /*
    * display next month
    * @return void
@@ -213,11 +213,11 @@ function PerpetualCalendar(name) {
     d = this.get_param('date');
     d.setMonth(d.getMonth()+1);
     this.set_param('date', d);
-    
+
     /* update calendar display */
     this._update_calendarBody();
   };
-  
+
   /*
    * display previous month
    * @return void
@@ -228,11 +228,11 @@ function PerpetualCalendar(name) {
     d = this.get_param('date');
     d.setMonth(d.getMonth()-1);
     this.set_param('date', d);
-    
+
     /* update calendar display */
     this._update_calendarBody();
   };
-  
+
   /*
    * display given month
    * @param int month
@@ -246,11 +246,11 @@ function PerpetualCalendar(name) {
     if (month>=0) d.setMonth(month);
     if (year) d.setYear(year);
     this.set_param('date', d);
-    
+
     /* update calendar display */
     this._update_calendarBody();
   };
-  
+
   /*
    * return current display date
    * @return date
@@ -259,7 +259,7 @@ function PerpetualCalendar(name) {
   this.get_displayedDate = function() {
     return this.get_param('date');
   };
-  
+
   /*
    * get item from day
    * @param integer day
@@ -270,7 +270,7 @@ function PerpetualCalendar(name) {
     date = this.get_param('date');
     return this.get_item(date, day);
   };
-  
+
   /*
    * get item from day
    * @param integer day
@@ -281,7 +281,7 @@ function PerpetualCalendar(name) {
     date = this.get_param('date');
     return this.get_items(date, day);
   };
-  
+
   /*
    * update calendar body
    * @return void
@@ -300,15 +300,15 @@ function PerpetualCalendar(name) {
     /* update month & year display */
     this.get_param('calendarDate').innerHTML = this.get_locale(this.get_param('monthes', date.getMonth()));
     this.get_param('calendarDate').innerHTML += ' '+date.getFullYear();
-    
+
     /* build first line */
     line = 0;
-    
+
     if ( date.getDay() > 0 ) {
       l = document.createElement('tr').cloneNode(true);
       l.id = this.name + 'Line' + (line++);
       this.get_param('calendarObject').appendChild(l);
-    
+
       /* add empty days ending last month */
       for(var i=0; i< date.getDay(); i++) {
         c = document.createElement('td').cloneNode(true);
@@ -316,7 +316,7 @@ function PerpetualCalendar(name) {
         l.appendChild(c);
       }
     }
-    
+
     /* add month days */
     while(date.getMonth() == this.get_param('date').getMonth()) {
       /* check for a new line */
@@ -325,17 +325,17 @@ function PerpetualCalendar(name) {
         l.id = this.name + 'Line' + (line++);
         this.get_param('calendarObject').appendChild(l);
       }
-      
+
       /* get day */
       day = date.getDate()
-      
+
       /* build cell */
       c = document.createElement('td').cloneNode(true);
       c.id = this.name + 'Cell' + day;
-      
+
       /* get day items */
       items = this.get_items(date);
-      
+
       /* check item list */
       if (items != false ) {
       	if (items.length > 1 && this.get_param('multiItem')['enabled'] == 1)
@@ -347,11 +347,11 @@ function PerpetualCalendar(name) {
       }
       c.innerHTML = day;
       l.appendChild(c);
-      
+
       /* update date value */
       date = new Date(date.getFullYear(), date.getMonth(), day+1, 1, 0, 0);
     }
-    
+
     /* add empty days start next month */
     if ( date.getDay() > 0 ) {
       for(var i=date.getDay(); i < 7; i++) {
@@ -361,7 +361,7 @@ function PerpetualCalendar(name) {
       }
     }
   };
-  
+
   /*
    * set cell properties
    * @param DOMElement cell
@@ -375,10 +375,10 @@ function PerpetualCalendar(name) {
       cell.className = 'cell'+item['class'];
     else
       cell.className = 'cell0';
-    
+
     if (item['hadEvents'] == 0)
       return;
-    
+
     /* check mouse click event */
     if ( item['clickEvent'] && item['clickEvent'] != '')
       click = item['clickEvent']+'('+day+');';
@@ -388,7 +388,7 @@ function PerpetualCalendar(name) {
       click = this.get_param('selectionCallBack') + '('+this.get_param('date').getFullYear()+','+(this.get_param('date').getMonth()+1)+','+day+');';
     else
       click = '';
-    
+
     /* check mouse over event */
     if ( item['overEvent'] && item['overEvent'] != '')
       over = item['overEvent']+'('+day+');';
@@ -398,7 +398,7 @@ function PerpetualCalendar(name) {
       over = '';
     if ( this.get_param('enableHighlight') == 1 && over != '')
       over += 'this.className=\'cell'+item['class']+'Over\';';
-      
+
     /* check mouse out event */
     if ( item['outEvent'] && item['outEvent'] != '')
       out = item['outEvent']+'('+day+');';
@@ -408,7 +408,7 @@ function PerpetualCalendar(name) {
       out = '';
     if ( this.get_param('enableHighlight') == 1 && out != '')
       out += 'this.className=\'cell'+item['class']+'\';';
-    
+
     if ( this.get_param('plateform') == 'ie' ) {
       /* add internet explorer events */
       if ( click != '')
@@ -417,7 +417,7 @@ function PerpetualCalendar(name) {
         cell.onmouseover = new Function(over);
       if ( out != '')
         cell.onmouseout = new Function(out);
-      
+
     } else {
       /* add real navigator events */
       if ( click != '' )
