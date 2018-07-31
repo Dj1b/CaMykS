3d85dc04877524e7a9635597c012b98ebf650152
JB Lebrun <jb@ideogram-design.fr>
2018-07-31 12:12:09 +0200

    Normalises files format, indents more nicely the code and makes the TinyMCEv3 input, ready to be parsed by Doxygen.

--- plugin/input/TinyMCEv3/js/configs/simple_cs.js
+++ plugin/input/TinyMCEv3/js/configs/simple_cs.js
@@ -1,43 +1,42 @@
-/*
- * CaMykS Engine
- * Developed by	    : camyks.net
- * Author	       	  : CaMykS Team <camyks.contact@gmail.com>
- * CaMykS Version   : 1.0b
- * Object Version   : 1.0
- * Object Type      : Plugin / Input Javascript
- * Creation Date		: Jul 2008
- * Last Modif Date	: Jul 2008
- * 
- * TinyMCEv3 input : TinyMCE 3 WebEditor Integration config file
+/**
+ * @brief TinyMCEv3 Input simple_cs configuration scripts
+ * @details Plugin / Input Javascripts
+ * @file plugin/input/TinyMCEv3/js/configs/simple_cs.js
+ * @author CaMykS Team
+ * @version 1.0
+ * @date Creation: Jul 2008
+ * @date Modification: Jul 2018
+ * @copyright 2008 - 2018 CaMykS Team
+ * @note This program is distributed as is - WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
- 
 tinyMCE.init({
-  mode : "textareas",
+    mode : "textareas",
     theme : "advanced",
-    
+
     /* enabled plugins */
     plugins : "safari,inlinepopups,camyksemaillink",
-    
+
     /* buttons lists */
     theme_advanced_buttons1 : "newdocument,cleanup,|,undo,redo,|,justifyleft,justifycenter,justifyright,justifyfull",
     theme_advanced_buttons1_add : ",|,formatselect,styleselect",
     theme_advanced_buttons2 : "bold,italic,underline,strikethrough,|,image,link,camyksemaillink,unlink,|,charmap",
     theme_advanced_buttons3 : "",
-    
+
     /* editor UI */
     theme_advanced_toolbar_location : "top",
     theme_advanced_toolbar_align : "left",
     theme_advanced_statusbar_location : "bottom",
     theme_advanced_resizing : false,
     apply_source_formatting : true,
-    
+
     /* blocks list */
     theme_advanced_blockformats : "p,div,blockquote,address,pre,h1,h2,h3,h4",
-    
+
     /* camyks params */
     editor_deselector : "mceNoEditor",
-    
+
     /* camyks information */
     language : camyks_language,
     content_css : camyks_css_file,
-    });
+});
--- plugin/input/TinyMCEv3/js/configs/textual.js
+++ plugin/input/TinyMCEv3/js/configs/textual.js
@@ -1,41 +1,40 @@
-/*
- * CaMykS Engine
- * Developed by	    : camyks.net
- * Author	       	  : CaMykS Team <camyks.contact@gmail.com>
- * CaMykS Version   : 1.0a
- * Object Version   : 1.0
- * Object Type      : Plugin / Input Javascript
- * Creation Date		: Jul 2008
- * Last Modif Date	: Jul 2008
- * 
- * TinyMCEv3 input : TinyMCE 3 WebEditor Integration config file
+/**
+ * @brief TinyMCEv3 Input textual configuration scripts
+ * @details Plugin / Input Javascripts
+ * @file plugin/input/TinyMCEv3/js/configs/textual.js
+ * @author CaMykS Team
+ * @version 1.0
+ * @date Creation: Jul 2008
+ * @date Modification: Jul 2018
+ * @copyright 2008 - 2018 CaMykS Team
+ * @note This program is distributed as is - WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
-
 tinyMCE.init({
-  mode : "textareas",
+    mode : "textareas",
     theme : "advanced",
-    
+
     /* enabled plugins */
     plugins : "safari",
-    
+
     /* buttons lists */
     theme_advanced_buttons1 : "newdocument,cleanup,|,undo,redo,|,bold,italic,underline,strikethrough",
     theme_advanced_buttons1_add : ",|,justifyleft,justifycenter,justifyright,justifyfull",
     theme_advanced_buttons2 : "",
     theme_advanced_buttons3 : "",
-    
+
     /* editor UI */
     theme_advanced_toolbar_location : "bottom",
     theme_advanced_toolbar_align : "center",
     theme_advanced_resizing : false,
     apply_source_formatting : true,
-    
+
     /* blocks list */
     theme_advanced_blockformats : "p,div",
-    
+
     /* camyks params */
     editor_deselector : "mceNoEditor",
-    
+
     /* camyks information */
     language : camyks_language,
-    });
+});
