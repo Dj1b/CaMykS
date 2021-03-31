ef768c8a670356653d04b7786cecd57ecf23a5bc
Dj1b <skymac.contact@gmail.com>
2021-03-31 09:53:19 +0200

    Updates CookieAgreementMessage to respect new GDPR rules with cookie management.

--- plugin/input/CookieAgreementMessage/js/CookieAgreementMessage.js
+++ plugin/input/CookieAgreementMessage/js/CookieAgreementMessage.js
@@ -1,24 +1,24 @@
 /**
  * @brief CookieAgreementMessage Input scripts
  * @details Plugin / Input Javascripts
- * @file plugin/input/CookieAgreementMessage/js/CookieAgreementMessage.js
  * @author CaMykS Team
- * @version 1.0
+ * @version 1.1.0
  * @date Creation: May 2017
- * @date Modification: Apr 2018
- * @copyright 2017 - 2018 CaMykS Team
+ * @date Modification: Mar 2021
+ * @copyright 2017 - 2021 CaMykS Team
  * @note This program is distributed as is - WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 var CookieAgreementMessage = {
     params: {},
+    locales: {},
+    services: {},
 
-    /*
+    /**
      * add parameter
      * @param string name
      * @param mixed value
      * @return void
-     * @access public
      */
     set_param: function(param, value, subvalue) {
         if ( subvalue != undefined && this.params[param] )
@@ -27,11 +27,10 @@ var CookieAgreementMessage = {
             this.params[param] = value;
     },
 
-    /*
+    /**
      * return param value from name
      * @param mixed param
      * @return mixed
-     * @access public
      */
     get_param: function(param, value) {
         if (value != undefined && (this.params[param][value] || this.params[param][value] === 0))
@@ -41,22 +40,164 @@ var CookieAgreementMessage = {
         return false;
     },
 
-    /*
-     * initialise object
+    /**
+     * set locale value
+     * @param string name
+     * @param string value
      * @return void
-     * @access public
+     */
+    set_locale: function(name, value) {
+        this.locales[name.toLowerCase()] = value;
+    },
+
+    /**
+     * get locale value
+     * @param string name
+     * @return void
+     */
+    get_locale: function(name) {
+        if (this.locales[name.toLowerCase()])
+            return this.locales[name.toLowerCase()];
+        return name;
+    },
+
+    /**
+     * Initialise object
+     * @return void
      */
     initialise: function() {
 
     },
 
-    /*
-     * go to given anchor
-     * @param string anchor
-     * @access public
+    /**
+     * Expend panel.
+     * @return void
+     */
+    expand_panel: function() {
+        document.getElementById('CookieAgreementMessage').classList.remove('minimised');
+    },
+
+    /**
+     * Minimise panel.
+     * @return void
+     */
+    minimise_panel: function() {
+        document.getElementById('CookieAgreementMessage').classList.add('minimised');
+    },
+
+    /**
+     * Swap services selection display.
+     * @return void
+     */
+    swap_servicesSelectionDisplay: function() {
+        document.getElementById('CookieAgreementMessageServices').classList.toggle('opened');
+    },
+
+    /**
+     * Enable refresh button.
+     * @return void
+     */
+    enable_refreshButton: function() {
+        document.getElementById('CookieAgreementMessageRefreshButton').classList.remove('hidden');
+    },
+
+    /**
+     * Accept all cookies.
+     * @return void
      */
     accept_cookies: function() {
-        cookie_save('cookieAccepted', 1, this.get_param('cookieTTL'));
-        document.getElementById('cookieAgreementMessage').style.display = 'none';
+        for (var service in this.services) {
+            if (this.services[service] == 2)
+                continue;
+            this.services[service] = 1;
+            document.getElementById(service+'Accept').classList.add('selected');
+            document.getElementById(service+'Refuse').classList.remove('selected');
+        }
+        this.update_dataCookie();
+        this.enable_refreshButton();
+        if (!document.getElementById('CookieAgreementMessageServices').classList.contains('opened'))
+            this.refresh_page();
+    },
+
+    /**
+     * Refuse all cookies.
+     * @return void
+     */
+    refuse_cookies: function() {
+        for (var service in this.services) {
+            if (this.services[service] == 2)
+                continue;
+            this.services[service] = 0;
+            document.getElementById(service+'Accept').classList.remove('selected');
+            document.getElementById(service+'Refuse').classList.add('selected');
+        }
+        this.update_dataCookie();
+        this.enable_refreshButton();
+        if (!document.getElementById('CookieAgreementMessageServices').classList.contains('opened'))
+            this.refresh_page();
+    },
+
+    /**
+     * Accept one service.
+     * @param string service
+     * @return void
+     */
+    accept_serviceCookies: function(service) {
+        if (this.services[service] == 2)
+            return;
+        this.services[service] = 1;
+        document.getElementById(service+'Accept').classList.add('selected');
+        document.getElementById(service+'Refuse').classList.remove('selected');
+        this.update_dataCookie();
+        this.enable_refreshButton();
+    },
+
+    /**
+     * Refuse one service.
+     * @param string service
+     * @return void
+     */
+    refuse_serviceCookies: function(service) {
+        if (this.services[service] == 2)
+            return;
+        this.services[service] = 0;
+        document.getElementById(service+'Accept').classList.remove('selected');
+        document.getElementById(service+'Refuse').classList.add('selected');
+        this.update_dataCookie();
+        this.enable_refreshButton();
+    },
+
+    /**
+     * Update data cookie.
+     * @return void
+     */
+    update_dataCookie: function() {
+        data = JSON.stringify(this.services);
+        cookie_save('cookieAccepted', data, this.get_param('cookieTTL'));
+    },
+
+    /**
+     * Refresh page.
+     * @return void
+     */
+    refresh_page: function() {
+        document.location.reload();
+    },
+
+    /**
+     * Return unavailability message title.
+     * @return void
+     */
+    get_unavailabilityMessageTitle: function() {
+        return this.get_locale('unavailable_title');
+    },
+
+    /**
+     * Return unavailability message content.
+     * @param string service
+     * @return void
+     */
+    get_unavailabilityMessageContent: function(service) {
+        return this.get_locale('unavailable_content').replace('__SERVICE__', service);
     },
 }
