c4a3a7c0aa7df8fac4c6f0d57c08c5140cf78c40
JB Lebrun <jb@ideogram-design.fr>
2024-12-18 15:19:59 +0100

    Updates FPDF from version 1.82 to version 1.86.

--- engine/lib/external/FPDF/tutorial/makefont.php
+++ engine/lib/external/FPDF/tutorial/makefont.php
@@ -1,6 +1,6 @@
 <?php
-// Génération du fichier de définition de police pour le tutoriel 7
+// Génération des fichiers de police pour le tutoriel 7
 require('../makefont/makefont.php');
 
-MakeFont('calligra.ttf','cp1252');
+MakeFont('CevicheOne-Regular.ttf', 'cp1252');
 ?>
--- engine/lib/external/FPDF/tutorial/tuto7.php
+++ engine/lib/external/FPDF/tutorial/tuto7.php
@@ -1,11 +1,10 @@
 <?php
-define('FPDF_FONTPATH','.');
 require('../fpdf.php');
 
 $pdf = new FPDF();
-$pdf->AddFont('Calligrapher','','calligra.php');
+$pdf->AddFont('CevicheOne','','CevicheOne-Regular.php','.');
 $pdf->AddPage();
-$pdf->SetFont('Calligrapher','',35);
-$pdf->Cell(0,10,'Changez de police avec FPDF !');
+$pdf->SetFont('CevicheOne','',45);
+$pdf->Write(10,'Changez de police avec FPDF !');
 $pdf->Output();
 ?>
